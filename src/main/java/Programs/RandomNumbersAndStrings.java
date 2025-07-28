14095cf03cb25e4eac9c94c602eeaf203dfd8d81
raj <yadrajshinde1@gmail.com>
2025-07-28 16:59:36 +0530

    28th july

---
+++ src/main/java/Programs/RandomNumbersAndStrings.java
@@ -0,0 +1,22 @@
+package Programs;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
+
+
+public class RandomNumbersAndStrings {
+
+    public  static void main (String args[]){
+
+        System.out.println(RandomStringUtils.randomNumeric(10));
+        System.out.println(RandomStringUtils.randomAlphabetic(10));
+
+
+        Random random = new Random();
+        System.out.println(random.nextInt(100));
+        System.out.println(random.nextDouble());
+
+        System.out.println(Math.random());
+    }
+}
